d508429eaa6d7cce9ad4a9ef3269b219d1753035
Jun Park <jhpp114node@gmail.com>
2021-06-08 18:14:48 +0000

    added conditional statement practice with Alta3

---
+++ iftest/condition01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+hostname = "MTG"
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
+
+hostname = input("What value should we set for hostname?")
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    print("hostname matches expected config")
+
+print("Exiting the script")
---
+++ iftest2/ipask.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+ipchk = input("Apply an IP address: ")
+if ipchk == "192.168.70.1":
+    print("Looks like the IP address was set: " + ipchk + " not recommended..")
+elif ipchk:
+    print("Lloks like the IP address was set: " + ipchk)
+else:
+    print("You did not provide input")
